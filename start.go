0ab074fb6d4b9ecc912587e245188da09c5f7f5a
Jeferson Machado <jefersonm@gmail.com>
2017-04-19 23:13:54 -0300

    Adding package dependency

--- start.go
+++ start.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/jefersonm/tech-linter/validation/lints"
+)
 
 func main() {
 	fmt.Printf("Starting Tech Linter\n")
+	lints.HasSpecificDependency("JUnit")
 }
